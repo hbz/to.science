385b8d888a01ef8460dd3b8709f947d71e844d8e
Peter Reimer <reimer@hbz-nrw.de>
2026-03-10 09:55:32 +0100

    first information about the crawler

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,7 +28,7 @@ author = 'Dr. Ingolf Kuss, Dr. Peter Reimer, Dr. Andres Quast, Jan Schnasse'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-"sphinx_rtd_theme",
+  "sphinx_rtd_theme",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
